c164b9efef5cb58f28349fb25c1e0580d42be636
Max Church <maxchurch01@gmail.com>
2022-08-26 17:52:25 +0100

    FINAL anti safari rounding fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ const Create: NextPage = () => {
                     newOptions[index] = e.target.value;
                     setOptions(newOptions);
                   }}
-                  className="block p-4 w-full text-gray-900 bg-gray-50 border-b border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="multi block p-4 w-full text-gray-900 bg-gray-50 border-b border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 ></input>
                 <button
                   onClick={() => {
